119117abd27c5a6e26815901221e9c1522616225
tsj845 <73963573+tsj845@users.noreply.github.com>
2020-11-05 18:45:17 -0500

    Update maze_data.py

--- maze_data.py
+++ maze_data.py
@@ -1,9 +1,27 @@
 import random
+import displayio
+
+## we need five sprites (ball, verticle passage, horizontal passage, corner, and a solid wall)
+# we only need one corner slot in the sheet because we can flip it (might change it later if it turns out to be too difficult)
+sprites = Bitmap(32, 32, 2)
+colors = Palette(2)
+colors[0] = 0x000000
+colors[1] = 0xFFFFFF
 
 class Marble:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+        self.sprite = displayio.TileGrid(sprites, tile_width=16, tile_height=16)
+    def move(self, direction, step):
+        if direction == 'up':
+            self.sprite.y -= step
+        elif direction == 'down':
+            self.sprite.y += step
+        elif direction == 'right':
+            self.sprite.x += 1
+        else:
+            self.sprite.x -= 1
 
 class Maze:
     def __init__(self, width, height):
