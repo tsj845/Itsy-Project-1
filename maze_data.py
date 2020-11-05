78f45250d40ad222a5fff5edb8c8243237f938de
vihaan842 <73970748+vihaan842@users.noreply.github.com>
2020-11-05 16:46:00 -0500

    Update maze_data.py
    adds extra stuff to maze init.

--- maze_data.py
+++ maze_data.py
@@ -9,4 +9,9 @@ class Maze:
     def __init__(self, width, height):
         self.width = width
         self.height = height
-        self.marble = Marble(random.randint(0, 15), random.randint(0, 15))
+        self.marble = Marble(random.randint(0, 15)*8, random.randint(0, 15)*8) #placed randomly, for now
+        self.direction = None #Starts as none, will represent what angle marble will move at (0-1)
+        self.speed = None #Starts as none, will represent pixels per second
+    def move_marble(self):
+        if self.direction != None and self.speed != None:
+            #TODO: Impliment moving of marble (probably will involve speed being hypotenuse of right triangle, direction being angle, cos and sin to calculate legs)
