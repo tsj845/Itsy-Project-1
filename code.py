cd35f07f5939f79e436db1f30ca222cbdce79c1f
tsj845 <73963573+tsj845@users.noreply.github.com>
2020-11-05 20:42:58 -0500

    Update code.py

--- code.py
+++ code.py
@@ -7,4 +7,19 @@ import pers
 import maze_data
 
 display = pers.display
-sense = pers.something
+sense = pers.sensor
+
+class storeage():
+  pass
+
+app = storage()
+
+app.tilt = [0, 0]
+
+maze = maze_data.maze()
+
+for i in range(60):
+  app.tilt[0] += sense.rotation[0]
+  app.tilt[1] += sense.rotation[1]
+  maze.move(app.tilt)
+  time.sleep(0.25)
