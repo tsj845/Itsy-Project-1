48376a39c8ac19fd78a6982c8bd72ae7a51cfa31
tsj845 <73963573+tsj845@users.noreply.github.com>
2020-11-05 19:48:00 -0500

    Update pers.py

--- pers.py
+++ pers.py
@@ -4,9 +4,17 @@ import adafruit_ssd1327
 import busio
 import time
 
+i2c = busio.I2C(board.SCL, board.SDA)
+
+### these can be un-commented after Tristan is able to attach the IMU to the itsy ###
+######################################
+#from adafruit_lsm6ds import LSM6DS33#
+#sensor = LSM6DS33(i2c)###############
+######################################
+### do not un-comment the above lines ###
+
 displayio.release_displays()
 
-i2c_bus = busio.I2C(board.SCL, board.SDA)
-display_bus = displayio.I2CDisplay(i2c_bus, device_address=0x3D)
+display_bus = displayio.I2CDisplay(i2c, device_address=0x3D)
 time.sleep(1)
 display = adafruit_ssd1327.SSD1327(display_bus, width=128, height=128)
