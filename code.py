1e3d6c4a75c58bb933ebc4803b1a29212de07fa2
tsj845 <73963573+tsj845@users.noreply.github.com>
2020-11-06 19:53:54 -0500

    Update code.py

--- code.py
+++ code.py
@@ -19,7 +19,7 @@ app.tilt = [0, 0]
 maze = maze_data.maze()
 
 for i in range(60):
-  app.tilt[0] += sense.rotation[0]
-  app.tilt[1] += sense.rotation[1]
+  app.tilt[0] += sense.gyro[0]
+  app.tilt[1] += sense.gyro[1]
   maze.move(app.tilt)
   time.sleep(0.25)
