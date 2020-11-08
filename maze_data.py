6b9b7427fd7015dc467aba53fb9215cabfece9bb
tsj845 <73963573+tsj845@users.noreply.github.com>
2020-11-08 15:51:01 -0500

    Update maze_data.py

--- maze_data.py
+++ maze_data.py
@@ -4,8 +4,6 @@ from displayio import *
 from adafruit_display_shapes.circle import Circle
 from adafruit_display_shapes.rect import Rect
 
-## we need five sprites (ball, verticle passage, horizontal passage, corner, and a solid wall)
-# we only need one corner slot in the sheet because we can flip it (might change it later if it turns out to be too difficult)
 sprites = Bitmap(32, 32, 2)
 colors = Palette(2)
 colors[0] = 0x000000
@@ -30,9 +28,7 @@ class Maze:
     def __init__(self, width, height, g):
         self.width = width
         self.height = height
-        self.marble = Marble(10, 10) #placed randomly, for now
-        self.direction = None #Starts as none, will represent what angle marble will move at (0-1)
-        self.speed = 0 #Starts as none, will represent pixels per second
+        self.marble = Marble(10, 10)
         g.append(self.marble.sprite)
     def move(self, tilt):
         direc1 = 'up'
@@ -51,9 +47,5 @@ class Maze:
             else:
                 direc2 = 'up'
             speed2 = round(tilt[1]+(1 * tilt[1]/abs(tilt[1])))
-        #change_x = math.sin(self.direction*2*math.pi)*self.speed
-        #change_y = math.cos(self.direction*2*math.pi)*self.speed
-        #self.marble.x += change_x
-        #self.marble.y += change_y
         self.marble.move(direc1, speed1)
         self.marble.move(direc2, speed2)
