0f3df46776528da0386ea567445b428f1f235e8a
tsj845 <73963573+tsj845@users.noreply.github.com>
2020-11-08 15:49:43 -0500

    Update pers.py

--- pers.py
+++ pers.py
@@ -20,7 +20,3 @@ sensor = LSM6DS33.LSM6DS33(i2c)###############
 display_bus = displayio.I2CDisplay(i2c, device_address=0x3D)
 time.sleep(1)
 display = adafruit_ssd1327.SSD1327(display_bus, width=128, height=128)
-
-g = displayio.Group()
-
-display.show(g)
