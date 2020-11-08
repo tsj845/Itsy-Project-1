3d624338177f3c82595becbe878c897b62bc2577
tsj845 <73963573+tsj845@users.noreply.github.com>
2020-11-08 16:44:13 -0500

    Update pers.py

--- pers.py
+++ pers.py
@@ -9,16 +9,8 @@ displayio.release_displays()
 i2c = busio.I2C(board.SCL, board.SDA)
 
 from adafruit_lsm6ds import LSM6DS33#
-<<<<<<< HEAD
-sensor = LSM6DS33.LSM6DS33(i2c)
-
-display_bus = displayio.I2CDisplay(i2c, device_address=0x3D)
-time.sleep(1)
-display = adafruit_ssd1327.SSD1327(display_bus, width=128, height=128)
-=======
 sensor = LSM6DS33.LSM6DS(i2c)
 
 display_bus = displayio.I2CDisplay(i2c, device_address=0x3D)
 time.sleep(1)
 display = adafruit_ssd1327.SSD1327(display_bus, width=128, height=128)
->>>>>>> c003bac094573cc24524a596767da8c613186229
