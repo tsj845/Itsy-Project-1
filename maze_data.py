8450462dcff7eda453c0b970626e788e3af3fee3
tsj845 <73963573+tsj845@users.noreply.github.com>
2020-11-08 22:58:38 -0500

    Update maze_data.py

--- maze_data.py
+++ maze_data.py
@@ -9,40 +9,61 @@ colors = Palette(2)
 colors[0] = 0x000000
 colors[1] = 0xFFFFFF
 
+for i in range(16):
+    for i2 in range(16):
+        sprites[i, i2] = 1
+
 class Marble:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+    def __init__(self, x, y, parent):
+        self.parent = parent
         self.sprite = Circle(x, y, 8, fill=0xFFFFFF, outline=0x000000)
+    def oOB(self, x, y):
+        for path in self.parent.paths:
+            if path[0] == 'c':
+                if y == int(path[1])*16:
+                    if x >= int(path[2])*16 and x < int(path[4])*16:
+                        return True
+            else:
+                if x == int(path[1])*16:
+                    if y >= int(path[2])*16 and y < int(path[4])*16:
+                        return True
+        return False
     def move(self, direction, step):
-        if direction == 'up' and self.sprite.y > 5:
-            self.sprite.y -= step
+        x = self.sprite.x
+        y = self.sprite.y
+        if direction == 'up' and self.sprite.y > 8:
+            y -= step
         elif direction == 'down' and self.sprite.y < 120:
-            self.sprite.y += step
+            y += step
         elif direction == 'right' and self.sprite.x < 120:
-            self.sprite.x += step
-        elif direction == 'left' and self.sprite.x > 5:
-            self.sprite.x -= step
+            x += step
+        elif direction == 'left' and self.sprite.x > 8:
+            x -= step
+        if self.oOB(x, y):
+            self.sprite.x = x
+            self.sprite.y = y
 
 class Maze:
     def __init__(self, width, height, g):
         self.width = width
         self.height = height
         self.tiles = TileGrid(sprites, pixel_shader = colors, width=8, height=8, tile_width=16, tile_height=16, default_tile=1)
-        self.marble = Marble(10, 10)
+        self.marble = Marble(8, 8, self)
         g.append(self.tiles)
         g.append(self.marble.sprite)
         self.paths = []
-    def createPath(self, x, y, d, h=False):
-        for i in range(d):
-            if h:
-                self.tiles[x+d, y] = sprites[0]
-            else:
-                self.tiles[x, y+d] = sprites[0]
-        if h:
-            self.paths.append((y, round(x*16-8), round((x+d)*16-8), True))
+    def clearPaths(self):
+        self.paths.clear()
+        for i in range(64):
+            self.tiles[i] = 1
+    def createPath(self, string):
+        self.paths.append(string)
+        if string[0] == 'c':
+            for i in range(int(string[2]), int(string[4])):
+                self.tiles[i+int(string[1])*8] = 0
         else:
-            self.paths.append((x, round(y*16-8), round((y+d)*16-8), False))
+            for i in range(int(string[2]), int(string[4])):
+                self.tiles[i*8+int(string[1])] = 0
     def move(self, tilt):
         direc1 = 'up'
         direc2 = 'up'
