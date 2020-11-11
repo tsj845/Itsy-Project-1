cfe1017c2aeb88bdab6fe4a4b5fae4cc168b8c32
tsj845 <73963573+tsj845@users.noreply.github.com>
2020-11-11 14:01:13 -0500

    Update code.py

--- code.py
+++ code.py
@@ -5,6 +5,15 @@ import busio
 from time import sleep
 import pers
 import maze_data
+from digitalio import DigitalInOut, Direction, Pull
+
+led = DigitalInOut(board.BLUE_LED)
+led.direction = Direction.OUTPUT
+
+switch = DigitalInOut(board.SWITCH)
+
+switch.direction = Direction.INPUT
+switch.pull = Pull.UP
 
 sense = pers.sensor# gets a reference to the sensor
 g = Group()# creates a group to be displayed
@@ -23,7 +32,13 @@ def logic():# does the work
   app.tilt[0] -= round(sense.gyro[0]*2)
   app.tilt[1] += round(sense.gyro[1]*2)
   maze.move(app.tilt)
-  sleep(0.25)
+  if not switch.value:
+    led.value = True
+    app.tilt = [0, 0]
+    sleep(0.25)
+    led.value = False
+  else:
+    sleep(0.25)
 
 def run(time=0):# this ensures that if you want unlimited testing time you must be connected via serial port
   if time == 0:#    this ensures that the tester can do a keyboard interrupt at any time
