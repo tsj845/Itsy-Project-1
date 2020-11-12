bb5e40b3799826d01481e15dc1a18bcc15b5d5ec
vihaan842 <73970748+vihaan842@users.noreply.github.com>
2020-11-12 12:40:45 -0500

    Update maze_data.py
    Math works

--- maze_data.py
+++ maze_data.py
@@ -1,39 +1,44 @@
 import random
 import math
-import displayio
+from displayio import *
 from adafruit_display_shapes.circle import Circle
 from adafruit_display_shapes.rect import Rect
 
-## we need five sprites (ball, verticle passage, horizontal passage, corner, and a solid wall)
-# we only need one corner slot in the sheet because we can flip it (might change it later if it turns out to be too difficult)
 sprites = Bitmap(32, 32, 2)
 colors = Palette(2)
 colors[0] = 0x000000
 colors[1] = 0xFFFFFF
 
-class Marble:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        self.sprite = Circle(x, y, 8)
-    def move(self, direction, step):
-        if direction == 'updown':
-            self.sprite.y += step
-            self.y += step
-        elif direction == 'leftright':
-            self.sprite.x += step
-            self.x += step
-            
+for i in range(16):# this generates the path tiles
+    for i2 in range(16):
+        sprites[i, i2] = 1
+    
 class Maze:
-    def __init__(self, width, height):
+    def __init__(self, width, height, g):
         self.width = width
         self.height = height
-        self.marble = Marble(random.randint(0, 15)*8, random.randint(0, 15)*8) #placed randomly, for now
-        self.direction = None #Starts as none, will represent what angle marble will move at (0-1)
-        self.speed = 1 #Placeholder of 1, will represent pixels per second
-    def move_marble(self):
+        self.tiles = TileGrid(sprites, pixel_shader = colors, width=8, height=8, tile_width=16,tile_height=16, default_tile=1)
+        self.marble = Circle(random.randint(0, 15)*8, random.randint(0, 15)*8, 8, fill=0xFFFFFF, outline=0x000000) #placed randomly, for now
+        self.speed_x = 1 #Placeholder of 1, will represent pixels per second
+        self.speed_y = 1
+        g.append(self.tiles)
+        g.append(self.marble)
+    def move_marble(self, tilt):
+        self.speed_x += 25*math.sin(tilt[0]*math.pi/180)
+        self.speed_y += 25*math.sin(tilt[1]*math.pi/180)
         if self.direction != None:
-            change_x = round(math.sin(self.direction*2*math.pi)*self.speed)
-            change_y = round(math.cos(self.direction*2*math.pi)*self.speed)
-            self.marble.move("leftright", change_x)
-            self.marble.move("updown", change_y)
+            change_x = round(math.sin(self.direction*2*math.pi)*self.speed_x)
+            change_y = round(math.cos(self.direction*2*math.pi)*self.speed_y)
+            self.marble.x += change_x
+            self.marble.y += change_y
+            if self.marble.x > 110:
+                self.marble.x = 110
+            if self.marble.x < 0:
+                self.marble.x = 0
+            if self.marble.y > 110:
+                self.marble.y = 110
+                self.speed_y = 0
+            if self.marble.y < 0:
+                self.marble.y = 0
+                self.speed_y = 0
+            print(tilt[1])
