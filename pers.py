c659880c782b3f64fb7d88fa58a7740ba27597e5
tsj845 <73963573+tsj845@users.noreply.github.com>
2020-11-13 16:34:23 -0500

    Update pers.py

--- pers.py
+++ pers.py
@@ -9,7 +9,7 @@ displayio.release_displays()# allows the i2c bus to be used
 i2c = busio.I2C(board.SCL, board.SDA)# initializes the i2c bus
 
 from adafruit_lsm6ds import LSM6DS33# imports the imu driver
-sensor = LSM6DS33.LSM6DS(i2c)# initializes the imu
+sensor = LSM6DS33.LSM6DS33(i2c)# initializes the imu
 
 display_bus = displayio.I2CDisplay(i2c, device_address=0x3D)# creates the display bus
 time.sleep(1)# this is an artifact of the example code on GitHub, i have not removed it because i don't know what its purpose is
