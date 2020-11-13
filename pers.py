16d194390d989ce6f18d1f97c96f1c18a2362263
tsj845 <73963573+tsj845@users.noreply.github.com>
2020-11-12 20:09:47 -0500

    Update pers.py

--- pers.py
+++ pers.py
@@ -8,4 +8,4 @@ release_displays()# allows the i2c bus to be used
 i2c = busio.I2C(board.SCL, board.SDA)# initializes the i2c bus
 
 from adafruit_lsm6ds import LSM6DS33# imports the imu driver
-sensor = LSM6DS33.LSM6DS(i2c)# initializes the imu
+sensor = LSM6DS33.LSM6DS33(i2c)# initializes the imu
