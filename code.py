9b6e0b53c06eb35548e0acd106a26b30f58a1ae4
tsj845 <73963573+tsj845@users.noreply.github.com>
2020-11-18 08:25:10 -0500

    Update code.py

--- code.py
+++ code.py
@@ -29,8 +29,8 @@ app.tilt = [0, 0]
 maze = maze_data.Maze(128, 128, g)
 
 def logic():# does the work
-  app.tilt[0] -= round(sense.gyro[0]*-2)
-  app.tilt[1] += round(sense.gyro[1]*-2)
+  app.tilt[0] -= round(sense.gyro[0]*2)
+  app.tilt[1] += round(sense.gyro[1]*2)
   maze.move_marble(app.tilt)
   if not switch.value:
     led.value = True
