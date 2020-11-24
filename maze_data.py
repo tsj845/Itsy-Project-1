49b6261421260c4eabb17ab39a4e8e70ce158324
Vihaan <vsj314@icloud.com>
2020-11-24 16:32:24 -0500

    comments

--- maze_data.py
+++ maze_data.py
@@ -40,44 +40,53 @@ class Maze:
         g.append(self.marble)
             
     def generate(self):
-        xs = []
-        ys = []
-        b = 0
-        x = 0
-        y = 0
-        branches = 0
+        xs = [] #stores prier x coords
+        ys = [] #stores prier y coords
+        b = 0 #stores how far back to go in list
+        x = 0 #stores current x
+        y = 0 #stores current y
+        branches = 0 #stores number of branches created
         self.board[0][0] = True
         while True:
             if b==0:
+                #if 0 back, add x and y to lists
                 xs.append(x)
                 ys.append(y)
             else:
+                #otherwise, go back b x and b y
                 x = xs[len(xs)-b-1]
                 y = ys[len(ys)-b-1]
-            failed = True
-            direction = random.randint(0, 4)
+            failed = True #to test if it fails or not
+            direction = random.randint(0, 4) #which direction to "move"
+            #checks if direction is one way and if a square there would touch other squares
             if (direction==0 or direction==4) and x+1<7 and not self.rboard(x+1, y) and not self.rboard(x+2, y) and not self.rboard(x+1, y+1) and not self.rboard(x+1, y-1):
-                x += 1
-                self.board[x][y] = True
-                failed = False
+                x += 1 #if it succeeds, change x
+                self.board[x][y] = True #set the square as a path
+                failed = False #did not fail
+            #if it has reached the edge
             elif (direction==0 or direction==4) and x+1==7:
-                count = 0
+                count = 0 #number of paths
                 for i in self.board:
                     for j in i:
                         if j:
                             count += 1
+                #if this is the first branch
                 if branches==0:
+                    #add final square
                     x += 1
                     self.board[x][y] = True
                     xs.append(x)
                     ys.append(y)
-                    branches += 1
-                    b = random.randint(0, len(xs)-1)
+                    branches += 1 #increase branches
+                    b = random.randint(0, len(xs)-1) #set random start for new branch
                     continue
+                #if the count is less than half of the board
                 elif count<32:
+                    #continue to next branch
                     branches += 1
                     b = random.randint(0, len(xs)-1)
                     continue
+                #otherwise, stop
                 else:
                     break
             elif direction==1 and y+1<8 and not self.rboard(x, y+1) and not self.rboard(x, y+2) and not self.rboard(x+1, y+1) and not self.rboard(x-1, y+1):
@@ -92,10 +101,13 @@ class Maze:
                 y -= 1
                 self.board[x][y] = True
                 failed = False
+            #if it failed, change b so next time it goes back in the lists
             if failed:
                 b += 1
+                #if b is longer than list, go back to beginning and try again
                 if b == len(xs):
                     b = 0
+            #if it didn't fail and b isn't 0, change it to 0
             elif b != 0:
                 b = 0
     def rboard(self, x, y):
