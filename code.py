17466153e0da40ea5286244bf66119348bc99e3f
tsj845 <73963573+tsj845@users.noreply.github.com>
2020-12-02 15:48:01 -0500

    Update code.py

--- code.py
+++ code.py
@@ -19,10 +19,17 @@ app.tilt = [0, 0]# stores the tilt on the x and y axes
 
 maze = maze_data.Maze(128, 128, g)# creates the maze
 
+m = 1 #for callibration of sensitivity
+limiter = 5
+
 while True:
-  if 5*sense.gyro[0] > 0.5:
-    app.tilt[0] -= 5*sense.gyro[0]
-  if 5*sense.gyro[0] > 0.5:
-    app.tilt[1] += 5*sense.gyro[1]
+  if m*sense.gyro[0] > 0.5:
+    app.tilt[0] -= m*sense.gyro[0]
+  if m*sense.gyro[1] > 0.5:
+    app.tilt[1] += m*sense.gyro[1]
+  if app.tilt[0] > limiter:
+    app.tilt[0] = limiter
+  if app.tilt[1] > limiter:
+    app.tilt[1] = limiter
   maze.move_marble(app.tilt)
   time.sleep(0.01)
