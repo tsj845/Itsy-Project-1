5ac3a3b40029aec4dd54e8ccab09fc1be7a62a98
tsj845 <73963573+tsj845@users.noreply.github.com>
2020-12-05 16:14:02 -0500

    Update code.py

--- code.py
+++ code.py
@@ -11,12 +11,23 @@ sense = pers.sensor# gets a reference to the sensor
 g = Group()# creates a group to be displayed
 pers.display.show(g)# displays the group
 
+l = maze_text.Label(maze_text.font, x=64, y=64, color=0xFFFFFF, text="hello, world!", max_glyphs=1000)
+
+l.anchor_point = (0.5, 0.5)
+
+l.anchored_position = (64, 64)
+
 class storeageC():# helps to prevent unbound local errors
   pass
 
 app = storeageC()# creating an instance of the storage class
 
 app.tilt = [0, 0]# stores the tilt on the x and y axes
+
+g.append(l)
+
+#while True:
+#  pass
 
 #maze = maze_data.Maze(128, 128, g)# creates the maze
 
