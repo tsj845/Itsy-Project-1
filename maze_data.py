6eef2b966410beda43f4d696675e9ee463fe39d8
tsj845 <73963573+tsj845@users.noreply.github.com>
2020-12-05 18:30:13 -0500

    Update maze_data.py

--- maze_data.py
+++ maze_data.py
@@ -1,99 +1,170 @@
-from random import seed, randrange
-import math
+import random
+from math import sin, cos, pi
+from time import sleep
 from displayio import *
 from adafruit_display_shapes.circle import Circle
-from adafruit_display_shapes.rect import Rect
+#from adafruit_display_shapes.rect import Rect## we aren't using it right now so no need to import it
 
-sprites = Bitmap(32, 32, 2)# creates the spritesheet
-colors = Palette(2)# creates the color palette
-colors[0] = 0x000000# fills in the color palette
+sprites = Bitmap(32, 32, 2)
+colors = Palette(2)
+colors[0] = 0x000000
 colors[1] = 0xFFFFFF
 
 for i in range(16):# this generates the path tiles
     for i2 in range(16):
         sprites[i, i2] = 1
 
-class Marble:
-    def __init__(self, x, y, parent):
-        self.parent = parent
-        self.sprite = Circle(x, y, 8, fill=0xFFFFFF, outline=0x000000)# creates the marble drawn onscreen
-    def onLoseGame(self):
-        self.gameOver.hidden = False
-    def oOB(self, x, y):
-        for path in self.parent.paths:# checks all of the paths
-            if path[0] == 'c':# this checks the orientation of the path
-                if y == int(path[1])*16:# this makes sure that the y coordinate is in the path at all
-                    if x >= int(path[2])*16 and x < int(path[4])*16:# checks to make sure that the x coordinate is valid
-                        return True
-            else:
-                if x == int(path[1])*16:# these do the same tasks as the lines above except that the x and y checking is swapped
-                    if y >= int(path[2])*16 and y < int(path[4])*16:
-                        return True
-        return False
-    def move(self, direction, step):
-        x = self.sprite.x# this provides a value that won't affect the sprite if changed
-        y = self.sprite.y
-        if direction == 'up' and self.sprite.y > 8:# these if statements ensure that the marble isn't going to go off the screen
-            y -= step# the x and y coordinates of the sprite are not changed until later so that they can be checked
-        elif direction == 'down' and self.sprite.y < 120:
-            y += step
-        elif direction == 'right' and self.sprite.x < 120:
-            x += step
-        elif direction == 'left' and self.sprite.x > 8:
-            x -= step
-        if self.oOB(x, y):# ensures that the x and y coordinates are valid before applying them to the marble
-            self.sprite.x = x
-            self.sprite.y = y
-
 class Maze:
-    def __init__(self, width, height, g):
+    def __init__(self, g):
         self.width = width
         self.height = height
         self.tiles = TileGrid(sprites, pixel_shader = colors, width=8, height=8, tile_width=16,
                               tile_height=16, default_tile=1)
-        # self.tiles stores the color values for the maze
-        self.marble = Marble(8, 8, self)# creates a marble
-        g.append(self.tiles)# renders the maze and marble
-        g.append(self.marble.sprite)
-        self.paths = []# this is the list of all the paths
-        self.goal = (7, 7)# the goal for the maze (will contain a seperate texture later)
-    def checkWin(self):# currently doesn't trigger anything
-        ## checks to see if the sprite is within twelve (12) pixels of the center coordinates of the tile marked as the goal
-        win = False
-        if abs(self.goal[0]*16 - self.marble.sprite.x) < 16 and abs(self.goal[1]*16 - self.marble.sprite.y) < 16:
-            win = True
-        print(win)
-    def generateMaze(self):
+        self.marble = Circle(8, 8, 8, fill=0xFFFFFF,
+                             outline=0x000000) #placed at (0, 0)
+        #self.speed_x = 1 #Placeholder of 1, will represent pixels per second
+        #self.speed_y = 1
+        self.goal = (7, 7)
+        self.threshold = 2
+        self.hole = Circle(self.goal[0]*16+8, self.goal[1]*16+8, 8, fill=0x777777, outline=0xDDDDDD)
+        g.append(self.tiles)
+        g.append(self.hole)
+        g.append(self.marble)
+        self.paths = []
+        self.dA = False
+        self.mode = 0
+        self.limit = 8
+    def dAn(self):
+        if self.mode == 1:
+            for i in range(15):
+                self.marble.fill -= 0x111111
+                sleep(0.5)
+            self.reset()
+    def reset(self, v=False):
+        self.marble.x = 0
+        self.marble.y = 0
+        self.marble.fill = 0xFFFFFF
+        self.dA = False
+        if v:
+            self.clearPaths()
+            self.generateMaze()
+    def setMode(self, mode):
+        self.mode = mode
+    def checkWin(self):
+        xv = abs(self.goal[0]*16 - self.marble.x)
+        yv = abs(self.goal[1]*16 - self.marble.y)
+        if xv < 8 and yv < 8:
+            self.reset(True)
+    def generateMaze(self, nPaths=5):
+        """
+        should generate the maze
+        should set the exit position by changing the goal variable
+        once the exit is set run this code:
+            self.hole.x = self.goal[0]*16
+            self.hole.y = self.goal[1]*16
+        end of code
+        """
         pass
-    def clearPaths(self):# this is a debugging tool, may be usefull for generating new mazes after-
-        self.paths.clear()## old ones are beaten by the player
-        for i in range(64):# sets all the tiles to walls and clears the list of paths
+    def clearPaths(self):
+        self.paths.clear()
+        for i in range(64):
             self.tiles[i] = 1
-    def createPath(self, string):# syntax: "txy-z"
-        self.paths.append(string)# puts the string used to generate the path in the list of paths
-        if string[0] == 'c':# checks the orientation of the path
-            for i in range(int(string[2]), int(string[4])):# sets the colors
+    def createPath(self, string):
+        """
+        syntax:
+        'txy-z'
+        t = type -> 'r' or 'c'
+        x = pos1 -> if 'r' then x coord, if 'c' then y coord
+        y = pos2 -> if 'r' then starting row, if 'c' then starting col
+        z = dist -> how many tiles in the path
+        """
+        self.paths.append(string)
+        if string[0] == 'c':
+            for i in range(int(string[2]), int(string[4])):
                 self.tiles[i+int(string[1])*8] = 0
         else:
             for i in range(int(string[2]), int(string[4])):
                 self.tiles[i*8+int(string[1])] = 0
-    def move(self, tilt):# allows the marble to move through the maze
-        direc1 = 'up'# "up" is a placeholder, there are two direction and speed variables because-
-        direc2 = 'up'## there are two axes
-        speed1 = 0
-        speed2 = 0
-        if abs(tilt[0]) > 3:# provides a thresh hold for movement
-            if tilt[0] > 0:# checks if the player has tilted the maze right or left
-                direc1 = 'right'# sets the direction variable accordingly
+    def checkBounds(self, x, y):
+        good = False
+        if self.mode != 1:
+            for path in self.paths:
+                if path[0] == 'c':
+                    if y == int(path[1])*16:
+                        if x >= int(path[2])*16 and x < int(path[4])*16:
+                            good = True
+                            break
+                else:
+                    if x == int(path[1])*16:
+                        if y >= int(path[2])*16 and y < int(path[4])*16:
+                            good = True
+                            break
+        if not good:
+            if self.mode == 1:
+                self.dA = True
+                good = True
+        return good
+    def move_marble(self, tilt):
+        """
+        direction value mapping:
+        0.0 : no x control, inverted y control
+        0.125 : inverted x & y controls
+        0.25 : inverted x control, no y control
+        0.375 : inverted x control, normal y control
+        0.5 : no x control, normal y control
+        0.657 : normal x & y controls
+        0.8 : normal x control, inverted y control
+        0.925 : normal x control, inverted y control
+        we can use these to change the controls and make things harder if we want
+        
+        i think this could be the way to go if you have any disagreements let me know
+        """
+        if self.dA:
+            self.dAn
+            return None
+        direction = 0.675# interesting thing is that if this changed the controls change
+        nx = self.marble.x + round(sin(direction*2*pi)*tilt[0])
+        ny = self.marble.y + round(cos(direction*2*pi)*tilt[1])
+        if nx % 2 != 0:
+            if nx < 0:
+                nx -= 1
             else:
-                direc1 = 'left'
-            speed1 = abs(round(tilt[0]))
-        if abs(tilt[1]) > 3:
-            if tilt[1] > 0:
-                direc2 = 'down'
+                nx += 1
+        if ny % 2 != 0:
+            if ny < 0:
+                ny -= 1
             else:
-                direc2 = 'up'
-            speed2 = abs(round(tilt[1]))
-        self.marble.move(direc1, speed1)# moves the marble
-        self.marble.move(direc2, speed2)
-        self.checkWin()# checks if the player has won
+                ny += 1
+        if abs(nx) > self.limit:
+            if nx < 0:
+                nx = self.limit*-1
+            else:
+                nx = self.limit
+        if abs(ny) > self.limit:
+            if ny < 0:
+                ny = self.limit*-1
+            else:
+                ny = self.limit
+        if nx < 0:
+            nx = 0
+        elif nx > 112:
+            nx = 112
+        if ny < 0:
+            ny = 0
+        elif ny > 112:
+            ny = 112
+        if ny > -1 and ny < 113:
+            if abs(ny - self.marble.y) > self.threshold:
+                if self.checkBounds(self.marble.x, ny):
+                    self.marble.y = ny
+        if nx > -1 and nx < 113:
+            if abs(nx - self.marble.x) > self.threshold:
+                if self.checkBounds(nx, self.marble.y):
+                    self.marble.x = nx
+                else:
+                    print('x bound')
+            else:
+                print('x threshold')
+        else:
+            print('x screen')
+        self.checkWin()
