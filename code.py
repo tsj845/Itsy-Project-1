436b28c73da60f07b7622f44a45f7255989d2257
tsj845 <73963573+tsj845@users.noreply.github.com>
2020-12-05 18:44:53 -0500

    Update code.py

--- code.py
+++ code.py
@@ -5,47 +5,56 @@ import busio
 import time
 import pers
 import maze_data
-import maze_text
+from digitalio import DigitalInOut, Direction, Pull
 
-sense = pers.sensor# gets a reference to the sensor
-g = Group()# creates a group to be displayed
-pers.display.show(g)# displays the group
+led = DigitalInOut(board.BLUE_LED)
+led.direction = Direction.OUTPUT
 
-#l = maze_text.Label(maze_text.font, x=64, y=64, color=0xFFFFFF, text="hello, world!", max_glyphs=1000)
+switch = DigitalInOut(board.SWITCH)
 
-#l.anchor_point = (0.5, 0.5)
+switch.direction = Direction.INPUT
+switch.pull = Pull.UP
 
-#l.anchored_position = (64, 64)
+sense = pers.sensor
+g = Group()
+pers.display.show(g)
 
-class storeageC():# helps to prevent unbound local errors
+class storeageC():
   pass
 
-app = storeageC()# creating an instance of the storage class
+app = storeageC()
 
-app.tilt = [0, 0]# stores the tilt on the x and y axes
+app.tilt = [0, 0]
 
-#g.append(l)
+maze = maze_data.Maze(128, 128, g)
 
-def test():
-  print('test')
+maze.createPath('c00-8')
 
-def test2():
-  print('text')
+def logic():# does the work
+  app.tilt[0] -= round(sense.gyro[0])*-2
+  app.tilt[1] += round(sense.gyro[1])*-2
+  maze.move_marble(app.tilt)
+  if not switch.value:
+    led.value = True
+    app.tilt = [0, 0]
+    time.sleep(0.25)
+  else:
+    led.value = False
+    time.sleep(0.25)
 
-bc = maze_text.menu(g, 8, 0, 16)
+def run(time=0):
+  if time == 0:
+    while True:
+      logic()
+  else:
+    for i in range(time*4):
+      logic()
 
-bc.addButton(text='print "test"', func=test)
-bc.addButton(text='print "text"', func=test2)
-
-#while True:
-#  pass
-
-#maze = maze_data.Maze(128, 128, g)# creates the maze
-
-
-
-#for i in range(60):
-  #app.tilt[0] -= sense.gyro[0]
-  #app.tilt[1] += sense.gyro[1]
-  #maze.move(app.tilt)
-  #time.sleep(0.25)
+def run2():
+  for i in range(10000):
+    if sense.gyro[0]*5>0.5 or sense.gyro[0]*5<-0.5:
+      app.tilt[0] -= 5*sense.gyro[0]
+    if sense.gyro[1]*5>0.5 or sense.gyro[1]*5<-0.5:
+      app.tilt[1] += 5*sense.gyro[1]
+    maze.move_marble(app.tilt)
+    time.sleep(0.01)
