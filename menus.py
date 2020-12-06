b484b028472439be58f5b46a4a6969d1cbc22239
tsj845 <73963573+tsj845@users.noreply.github.com>
2020-12-05 22:29:03 -0500

    Update menus.py

--- menus.py
+++ menus.py
@@ -1,1 +1,20 @@
 import maze_text
+from displayio import Group
+
+white = 0xFFFFFF
+black = 0x000000
+
+class mainMenu():
+    def __init__(self, g):
+        self.group = Group()
+        g.append(self.group)
+        self.mainMenu = maze_text.menu(self.group, 8, 0, 16)
+        self.videoSettings = maze_text.subMenu(self.group, 8, 0, 16, parent=self.mainMenu)
+        self.gameSettings = maze_text.subMenu(self.group, 8, 8, 16, parent=self.mainMenu)
+        self.mainMenu.addButton(text="video settings", func=self.videoSettings.show)
+        self.mainMenu.addButton(text="game settings", func=self.gameSettings.show)
+        self.videoSettings.addButton(text="return to main menu", func=self.hide)
+        self.videoSettings.addButton(text="toggle color mode (W.I.P)", func=None)
+        self.gameSettings.addButton(text="return to main menu", func=self.hide)
+    def move(self, direc):
+        maze_text.menuHandler.event(f'button_{direc}')
