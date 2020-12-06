066a3f3d05e73fcb05edb3f33920fd5669446b7d
tsj845 <73963573+tsj845@users.noreply.github.com>
2020-12-05 19:32:54 -0500

    Update group_utils.py
    added ability to change the maximum number of items within a group

--- group_utils.py
+++ group_utils.py
@@ -6,3 +6,12 @@ def setBack(g, index):
 
 def zIndex(g, index, value):
     g.insert(value, g.pop(index))
+
+def setMaxSize(group, size, parent=None):
+    g = Group(size)
+    for i in range(len(group)):
+        g.append(group.pop(0))
+    if parent != None:
+        i = parent.index(group)
+        parent[i] = g
+    return g
