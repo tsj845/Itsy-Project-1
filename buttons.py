12b3981f945bdba02bbabac6fe52cfbef2c87d7d
tsj845 <73963573+tsj845@users.noreply.github.com>
2020-12-05 20:26:29 -0500

    Update buttons.py

--- buttons.py
+++ buttons.py
@@ -2,10 +2,14 @@ import board
 import gamepad
 from digitalio import DigitalInOut, Direction, PULL
 
-pad = gamepad.GamePad(
-  DigitalInOut(board.A0),
-  DigitalInOut(board.A1),
-  DigitalInOut(board.A2),
-  DigitalInOut(board.A3),
-  DigitalInOut(board.A4)
-)
+class pad():
+    def __init__(self):
+        self.pad = gamepad.GamePad(
+            DigitalInOut(board.A0),
+            DigitalInOut(board.A1),
+            DigitalInOut(board.A2),
+            DigitalInOut(board.A3),
+            DigitalInOut(board.A4)
+            )
+    def eventCheck(self):
+        print(self.pad.get_pressed())
