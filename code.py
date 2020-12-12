5a2d9ae468b6abb29c9c5a8e8aebd42bb7301a76
tsj845 <73963573+tsj845@users.noreply.github.com>
2020-12-11 21:12:54 -0500

    Update code.py

--- code.py
+++ code.py
@@ -6,6 +6,9 @@ import time
 import pers
 import maze_data
 
+vec2 = maze_data.vec2
+bsdf = maze_data.boxSDF
+
 sense = pers.sensor# gets a reference to the sensor
 g = Group()# creates a group to be displayed
 pers.display.show(g)# displays the group
@@ -18,15 +21,23 @@ app = storeageC()# creating an instance of the storage class
 app.tilt = [0, 0]# stores the tilt on the x and y axes
 
 maze = maze_data.Maze(g)# creates the maze
-t1 = time.monotonic()
-maze.move_marble([0, 0])
-tDif = time.monotonic() - t1
+#t1 = time.monotonic()
+#maze.move_marble([0, 0])
+#tDif = time.monotonic() - t1
 
-tDel = round(tDif, 2)+0.2
+#tDel = round(tDif, 2)+0.2
 #print(maze.getTiles())
-t = maze.getTiles()
-print(maze.checkTiles(t))
-print(maze.collisionCheck(maze.checkTiles(t)))
+#t = maze.getTiles(maze.marble.x, maze.marble.y)
+#print(maze.checkTiles(t, maze.marble.x, maze.marble.y))
+#print(maze.collisionCheck(maze.checkTiles(t, maze.marble.x, maze.marble.y)))
+def cc(x, y):
+    maze.marble.x = x
+    maze.marble.y = y
+    t = maze.getTiles(maze.marble.x, maze.marble.y)
+    l = maze.checkTiles(t, maze.marble.x, maze.marble.y)
+    print(t)
+    print(l)
+    return maze.collisionCheck(l)
 """
 while True:
   if sense.gyro[0]*3>0.5 or sense.gyro[0]*3<-0.5:
@@ -34,5 +45,5 @@ while True:
   if sense.gyro[1]*3>0.5 or sense.gyro[1]*3<-0.5:
     app.tilt[1] += 3*sense.gyro[1]
   maze.move_marble(app.tilt)
-  time.sleep(tDel)
+  time.sleep(0.1)
 """
